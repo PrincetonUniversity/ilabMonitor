0952c19f8166e10e31d116b9b74bb9e7bc9e7051
mcahn <Matthew.Cahn@gmail.com>
2019-12-09 17:20:20 +0000

    Add the name of the mail server to the config file. Pass the mail server name around as needed. Update the nanny script to use the yaml config file instead of command-line arguments.

--- ilabsMonitorNanny.sh
+++ ilabsMonitorNanny.sh
@@ -17,7 +17,9 @@ if [[ -z $pid ]]; then
     tail -1 $nanny_logfile | mail -s "ilabs monitor restarted" mcahn@princeton.edu
 
     cd $dir
-    nohup ilabsMonitor.py -r mcahn@princeton.edu -r glaevsky@princeton.edu -r cdecoste@princeton.edu -r kr7@princeton.edu -r azerdoum@princeton.edu -r ilab-support@agilent.com> $monitor_stdout 2>&1 &
+    nohup ilabsMonitor.py --config ilabs-config-test.yaml --verbose > $monitor_stdout 2>&1 &
+    
+    ## nohup ilabsMonitor.py -r mcahn@princeton.edu -r glaevsky@princeton.edu -r cdecoste@princeton.edu -r kr7@princeton.edu -r azerdoum@princeton.edu -r ilab-support@agilent.com> $monitor_stdout 2>&1 &
 
 else
     words=( $pid )
