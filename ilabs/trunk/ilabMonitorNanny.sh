81b619c269ceb0de1061f4a6f02c16a27c291757
mcahn <mcahn@fdae9824-a4da-42c1-b03e-d6246b4cd2ae>
2019-12-09 17:42:03 +0000

    Change all the "ilabs" references and filenames to "ilab".

---
+++ ilabs/trunk/ilabMonitorNanny.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export PATH=".:/molbio2/mcahn/anaconda3-python3.5.2/bin:$PATH"
+
+dir=/molbio2/mcahn/ilabs
+
+monitor_stdout=/var/log/ilab/ilabMonitor-stdout.log
+nanny_logfile=/var/log/ilab/ilabMonitorNanny.log
+
+pid=`pgrep -f 'python.*ilabMonitor.py'`
+
+echo -n `date "+%Y-%m-%d %H:%M:%S"`": " >> $nanny_logfile
+
+if [[ -z $pid ]]; then
+    echo The iLab monitor is not running, starting it. >> $nanny_logfile
+
+    tail -1 $nanny_logfile | mail -s "iLab monitor restarted" mcahn@princeton.edu
+
+    cd $dir
+    nohup ilabMonitor.py --config ilab-config-test.yaml --verbose > $monitor_stdout 2>&1 &
+    
+    ## nohup ilabsMonitor.py -r mcahn@princeton.edu -r glaevsky@princeton.edu -r cdecoste@princeton.edu -r kr7@princeton.edu -r azerdoum@princeton.edu -r ilab-support@agilent.com> $monitor_stdout 2>&1 &
+
+else
+    words=( $pid )
+    if [ ${#words[@]} == 1 ]; then
+        echo The iLab monitor is running. >> $nanny_logfile
+    else
+        echo ${#words[@]} iLab monitors are running.  There should only be one. >> $nanny_logfile
+
+        tail -1 $nanny_logfile | mail -s "multiple iLab monitors running" mcahn@princeton.edu
+    fi
+fi
+
